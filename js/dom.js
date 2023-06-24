f27b1a364c5d5d216ee141ec1b6901d2e7588357
CamiloXD1206 <cetacue@misena.edu.co>
2023-06-23 12:15:58 -1200

    hola mundo

---
+++ js/dom.js
@@ -0,0 +1,8 @@
+function login() {
+    const usu = document.getElementById('username').value;
+    const pass = document.getElementById('password').value;
+    if (usu == 'adso2556678' && pass == 'adso2023') {
+        window.location.href = 'index.html';
+
+    }
+}
---
+++ js/funciones.js
@@ -0,0 +1,8 @@
+//--------------------------------------------------------------
+function ola() {
+    alert('debes iniciar sesion o registrarte para acceder');
+}
+
+function comentarios() {
+    alert('Tu PQRS se envio con exito dentro de los proximos 3 dias recibiras tu respuesta');
+}
