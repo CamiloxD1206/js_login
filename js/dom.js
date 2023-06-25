32c22c1987148d1372621b393d477f8598633645
CamiloXD1206 <cetacue@misena.edu.co>
2023-06-24 19:32:01 -1200

    se termino la validacion del login

--- js/dom.js
+++ js/dom.js
@@ -1,8 +1,27 @@
-function login() {
-    const usu = document.getElementById('username').value;
-    const pass = document.getElementById('password').value;
-    if (usu == 'adso2556678' && pass == 'adso2023') {
-        window.location.href = 'index.html';
+ //-----------------------------------------
+ function login() {
+     const usu = document.getElementById('username').value;
+     const pass = document.getElementById('password').value;
+     if (usu == 'adso2556678' && pass == 'adso2023') {
+         alert('inicio de sesion exitoso')
+         window.location = 'inicio_usumv.html'
 
-    }
-}
+     }
+ }
+ //-------------------------------------------------
+ const modal = document.querySelector('.sesion');
+ const open_modal = document.querySelector('.centro');
+ const modal_close = document.querySelector('.cerrar')
+ modal.addEventListener('click', (z) => {
+     z.preventDefault()
+
+     open_modal.classList.add('centro_show')
+
+ })
+
+ modal_close.addEventListener('click', (z) => {
+     z.preventDefault()
+
+     open_modal.classList.remove('centro_show')
+
+ })
